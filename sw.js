b726d416519f4401024716312b6606dc6809468b
“MansourTarekMansour <mansourtarek100@gmail.com>
2026-08-15 22:16:44 +0300

    Updates

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.__precacheManifest = [
     "url": "framework-1c199377d5421f8e7ab4.js"
   },
   {
-    "url": "app-11fa5334309f6d65cf0e.js"
+    "url": "app-5c6c4ff6c382c07851c0.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2be5db1dfd608ca867b2da5e26e31af1"
+    "revision": "e7339211ca0d49fa6412379787e3cdaa"
   },
   {
     "url": "polyfill-543b336484eed5784f5d.js"
@@ -136,7 +136,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-11fa5334309f6d65cf0e.js`))) {
+  if (!resources || !(await caches.match(`/app-5c6c4ff6c382c07851c0.js`))) {
     return await fetch(event.request)
   }
 
